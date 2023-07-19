bd026fddadf84de2db56555f5f12abdd4daa628c
bongjune1~ <darksen7693@gmail.com>
2023-07-19 16:02:45 +0900

    최종

--- src/main/java/shop/mtcoding/mall/controller/ProductController.java
+++ src/main/java/shop/mtcoding/mall/controller/ProductController.java
@@ -1,18 +1,41 @@
 package shop.mtcoding.mall.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import shop.mtcoding.mall.model.Product;
+import shop.mtcoding.mall.model.ProductRepository;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.util.List;
 
 @Controller
 public class ProductController {
 
+    @Autowired
+    private ProductRepository productRepository;
+
     @GetMapping("/")
-    public String home(){
+    public String home(HttpServletRequest request) {
+        List<Product> productList = productRepository.findAll();
+        request.setAttribute("productList",productList);
         return "home";
     }
 
     @GetMapping("/write")
-    public String write(){
+    public String writePage() {
         return "write";
+    }
+
+    @PostMapping("/product")
+    public String write(String name, int price, int qty) throws IOException {
+        System.out.println("name: " + name);
+        System.out.println("price: " + price);
+        System.out.println("qty: " + qty);
+//        return "write";
+        productRepository.save(name, price, qty);
+        return "redirect:/";
     }
 }
